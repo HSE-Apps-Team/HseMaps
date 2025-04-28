bdb6c7b4eaae392de38cec5f637b4287301be966
RexfordTolle <tollerex000@hsestudents.org>
2025-04-27 22:06:31 -0400

    removed thing that wont let me merge die

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import { msalConfig } from './Auth/authConfig';
 
 (async () => {
     try {
-        await UtilityModule.verifyIP();
         await DataModule.initialize();
     } catch (error) {
         console.error('Application initialization failed:', error);
